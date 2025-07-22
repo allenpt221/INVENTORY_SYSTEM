14b7ade45b93aa6cf049b30663a6670e88917f5a
Allenpt221 <Allenpt26@gmail.com>
2025-07-23 03:37:20 +0800

    add some mobile responsive for the sidebar dropdown

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -31,6 +31,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 type AppSidebarProps = {
   username?: string;
@@ -48,6 +49,18 @@ const items = [
   { title: "dashboard", icon: LayoutDashboard , url: "/dashboard", requiresAdmin: true },
 ];
 
+function useIsMobile(breakpoint = 640) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < breakpoint);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [breakpoint]);
+
+  return isMobile;
+}
+
 
 
 
@@ -58,13 +71,18 @@ export function AppSidebar({ username, logout, DarkMode, toggleDarkMode, role }:
   const filteredItems = items.filter(
   (item) => !item.requiresAdmin || role?.toLowerCase() === "superadmin"
 );
-
+  
+  const isMobile = useIsMobile();
 
   return (
     <Sidebar className="flex flex-col h-full">
       <SidebarContent className="flex-1">
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel className="flex justify-between mb-3">
+            <img src="https://images.unsplash.com/photo-1637144113512-0fb2b860c10f?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""
+            className="w-5 h-5 rounded-full" />
+            <p>StockHub</p>
+          </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {filteredItems.map((item) => {
@@ -107,7 +125,7 @@ export function AppSidebar({ username, logout, DarkMode, toggleDarkMode, role }:
                   <ChevronUp className="ml-auto " />
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
-              <DropdownMenuContent side="right" className="mb-5">
+              <DropdownMenuContent side={isMobile ? "bottom" : "right"} className={`mb-2 sm:mb-5 ${isMobile ? "w-[15rem] max-w-xs" : "w-auto"}`}>
                 <DropdownMenuItem className="cursor-pointer">Account</DropdownMenuItem>
                 <DropdownMenuItem className="cursor-pointer">Billing</DropdownMenuItem>
                 <DropdownMenuItem className="cursor-pointer" onClick={toggleDarkMode}>{DarkMode ?  <span className="flex gap-2 items-center"><Moon /> Dark Mode</span> : <span className="flex gap-2 items-center"><Sun /> Light Mode</span> }</DropdownMenuItem>
