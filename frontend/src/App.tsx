c59e82cc757b6b28d7675fed9379b2da2c10b133
Allenpt221 <Allenpt26@gmail.com>
2025-07-23 00:28:29 +0800

    create the log in page and sign up page also create the axios getting the request came from the backend server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import LogIn from "./Pages/LogIn"
+import SignUp from "./Pages/Signup"
 
 function App() {
 
   return (
-    <div className="p-2">      
+    <div className="">      
       <Routes>
           <Route path="/" element={<LogIn />}/>
+          <Route path="/signup" element={<SignUp />}/>
+
       </Routes>
 
     </div>
--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -1,13 +1,13 @@
-import { LoginForm } from "./loginPage/login-form"
+import { SignupForm } from "./signupPage/signup-form"
 
-function LogIn() {
+function SignUp() {
     return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
         <div className="w-full max-w-sm md:max-w-3xl">
-            <LoginForm />
+            <SignupForm />
         </div>
     </div>
     )
 }
 
-export default LogIn
+export default SignUp
