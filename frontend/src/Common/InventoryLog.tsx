69c29e14bfbab8c8d1815f019b729cd7f74092cf
Allenpt221 <Allenpt26@gmail.com>
2025-11-12 21:29:12 +0800

    change the direction of the arrow if the total is less than before

--- frontend/src/Common/InventoryLog.tsx
+++ frontend/src/Common/InventoryLog.tsx
@@ -119,20 +119,20 @@ export function InventoryLog() {
               ₱{formatNumberPrice(latest?.latestTotal ?? 0)}
               <span
                 className={`flex items-center gap-1 text-sm font-medium ${
-                  (latest?.beforetotal ?? 0) > (latest?.latestTotal ?? 0)
+                  (latest?.latestTotal ?? 0) > (latest?.beforetotal ?? 0)
                     ? "text-green-500"
                     : (latest?.latestTotal ?? 0) === (latest?.beforetotal ?? 0)
                     ? "text-green-500"
                     : "text-red-500"
                 }`}
               >
-                {(latest?.beforetotal ?? 0) > (latest?.latestTotal ?? 0) ? (
+                {(latest?.latestTotal ?? 0) > (latest?.beforetotal ?? 0) ? (
                   <ArrowUpRight size={16} />
-                ) : (latest?.beforetotal ?? 0) ===
-                  (latest?.latestTotal ?? 0) ? (
+                ) : (latest?.latestTotal ?? 0) ===
+                  (latest?.beforetotal ?? 0) ? (
                   <ArrowUpRight size={16} />
                 ) : (
-                  <ArrowDownRight size={16} />
+                    <ArrowDownRight size={16} />
                 )}
                 {latestPercentage.toFixed(2)}%
               </span>
@@ -147,14 +147,14 @@ export function InventoryLog() {
               {latest?.latestStock ?? 0}
               <span
                 className={`flex items-center gap-1 text-sm font-medium ${
-                  (latest?.beforestock ?? 0) > (latest?.latestStock ?? 0)
+                  (latest?.latestStock ?? 0) > (latest?.beforestock ?? 0)
                     ? "text-green-500"
-                    : (latest?.beforestock ?? 0) === (latest?.latestStock ?? 0)
+                    : (latest?.latestStock ?? 0) === (latest?.beforestock ?? 0)
                     ? "text-green-500"
                     : "text-red-500"
                 }`}
               >
-                {(latest?.beforestock ?? 0) > (latest?.latestStock ?? 0) ? (
+                {(latest?.latestStock ?? 0) > (latest?.beforestock ?? 0) ? (
                   <ArrowUpRight size={16} />
                 ) : (latest?.beforestock ?? 0) ===
                   (latest?.latestStock ?? 0) ? (
